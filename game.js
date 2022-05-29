a71e770e210150ff79b51b89d34e59f267ecae2a
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-05-28 20:09:15 -0400

    added objects for initial game logic

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-
+var EsotericNovice = {
+  name: "Nermok",
+  age: 21,
+  level: 1,
+  health: 140,
+  place: "First Floor: Books Of The Common",
+  weapon: "Echo Fusion Roccket",
+  weaponLevel: "1ST STAR",
+  items: "Generational Ring",
+};
+var Librarian = {
+  name: "Graham",
+  age: 32,
+  level: 3,
+  health: 80,
+  place: "First Floor: Books Of The Common",
+  weapon: "BOULDER CANNON",
+  weaponLevel: "1ST STAR",
+  items: "5TH EDITION: George Orwell 1984",
+};
